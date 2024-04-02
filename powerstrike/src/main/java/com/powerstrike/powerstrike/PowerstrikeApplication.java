3cf5a3adab51990a77ba201a9b8d3f566242916d
sandroex <sandroex_compras@hotmail.com>
2024-04-02 14:05:03 -0300

    [Alessandro] initial

---
+++ powerstrike/src/main/java/com/powerstrike/powerstrike/PowerstrikeApplication.java
@@ -0,0 +1,13 @@
+package com.powerstrike.powerstrike;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PowerstrikeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PowerstrikeApplication.class, args);
+	}
+
+}
---
+++ powerstrike/src/test/java/com/powerstrike/powerstrike/PowerstrikeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.powerstrike.powerstrike;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PowerstrikeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
